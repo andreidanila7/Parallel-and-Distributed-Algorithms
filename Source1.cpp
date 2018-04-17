e84661f3c16929659e7cb2c961d55a9e9078e56e
andreidanila7 <37181672+andreidanila7@users.noreply.github.com>
2018-04-17 15:33:13 +0300

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -1,70 +1,69 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
 using namespace std;
 mutex mtx;
+sem_t mutex;
 queue<int> Q;
 int p;
 condition_variable CondProd;
 condition_variable CondCons;
-
-void sem up()
-void sem down()
-
+  
 void Producer()
 {
-	while (true)
-	{
-		std::this_thread::sleep_for(std::chrono::seconds(3);
-
-		mtx.lock();
-		while (Q.size == 10)
-			CondProd.wait();
-
-
-
-		Q.push(1);
-		cout << "The product was added in que" << endl;
-		mtx.unlock();
-
-
-
-		CondCons.notify_all();
-	}
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(3);
+ 
+          sem_wait(&mutex);
+          while (Q.size == 10)
+   	         CondProd.wait();
+        
+          Q.push(1);
+          cout << "The product was added in que" << endl;
+          sem_post(&mutex);
+        
+        
+          CondCons.notify_all();
+    }
 }
-
+ 
 void Consumer()
 {
-	while (true)
-
-	{
-		mtx.lock();
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-
-		if (Q.size() == 0)
-		{
-			cout << "The queue is empty, please wait" << endl;
-			mtx.unlock();
-		}
-		else
-		{
-			Q.pop();
-			cout << "The product is out of the que." << endl;
-			mtx.unlock();
-		}
-		while (Q.size == 0)
-			CondCons.wait();
-		CondProd.notify_all();
-	}
-
+    while (true)
+ 
+    {
+        sem_wait(&mutex);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+ 
+        if (Q.size() == 0)
+        {
+            cout << "The queue is empty, please wait" << endl;
+            sem_post(&mutex);
+        }
+        else
+        {
+            Q.pop();
+            cout << "The product is out of the que." << endl;
+            sem_post(&mutex);
+        }
+        while (Q.size == 0)
+            CondCons.wait();
+        CondProd.notify_all();
+    }
+ 
 }
-
+ 
 int main()
 {
-	thread Prod(Producer);
-	thread Cons(Consumer);
-
-	Prod.join();
-	Cons.join();
-
-
-	system("pause");
-	return 0;
+    thread Prod(Producer);
+    thread Cons(Consumer);
+ 
+    Prod.join();
+    Cons.join();
+ 
+ 
+    system("pause");
+    return 0;
 }
